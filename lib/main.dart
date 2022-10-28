6e9f7bd8c055a9158225fc5d2191be9a8842841c
takashi0602 <t.takashi0602@gmail.com>
2022-10-28 17:22:57 +0900

    feat: create TopPage widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,37 @@
 import 'package:flutter/material.dart';
 
+import './poke_list_item.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pokedex',
+      title: 'Pokemon Flutter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: const TopPage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class TopPage extends StatelessWidget {
+  const TopPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Stack(
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(32),
-                  child: Image.network(
-                    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-                    height: 100,
-                    width: 100,
-                  ),
-                ),
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  child: const Text(
-                    'No.25',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const Text(
-              'pikachu',
-              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-            ),
-            const Chip(
-              label: Text('electric'),
-              backgroundColor: Colors.yellow,
-            ),
-          ],
+      body: SafeArea(
+        child: ListView.builder(
+          padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+          itemCount: 100,
+          itemBuilder: (context, index) => PokeListItem(index: index),
         ),
       ),
     );
