ee118512653a3327cc9d78c7f0db7927bbb897b6
takashi0602 <t.takashi0602@gmail.com>
2022-10-31 15:24:14 +0900

    feat: create ThemeMode selection page

---
+++ lib/theme_mode_selection_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class ThemeModeSelectionPage extends StatefulWidget {
+  const ThemeModeSelectionPage({super.key, required this.initThemeMode});
+
+  final ThemeMode initThemeMode;
+
+  @override
+  State<ThemeModeSelectionPage> createState() => _ThemeModeSelectionPageState();
+}
+
+class _ThemeModeSelectionPageState extends State<ThemeModeSelectionPage> {
+  ThemeMode currentThemeMode = ThemeMode.system;
+
+  @override
+  void initState() {
+    super.initState();
+    currentThemeMode = widget.initThemeMode;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            ListTile(
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: () =>
+                    Navigator.pop<ThemeMode>(context, currentThemeMode),
+              ),
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.system,
+              groupValue: currentThemeMode,
+              title: const Text('System'),
+              onChanged: (val) => {setState(() => currentThemeMode = val!)},
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.dark,
+              groupValue: currentThemeMode,
+              title: const Text('Dark'),
+              onChanged: (val) => {setState(() => currentThemeMode = val!)},
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.light,
+              groupValue: currentThemeMode,
+              title: const Text('Light'),
+              onChanged: (val) => {setState(() => currentThemeMode = val!)},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
